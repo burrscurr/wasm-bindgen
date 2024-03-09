6a8321043b26f7e9829b6ea068b7b48a6d751b44
daxpedda <daxpedda@gmail.com>
2024-03-09 00:21:46 +0000

    Deploying to gh-pages from @ rustwasm/wasm-bindgen@ad4e4435e310dcebe6a14be34ef027e916306fb1 🚀

---
+++ exbuild/websockets/pkg/websockets_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start_websocket(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke0_mut__h01379f57e7fa370a(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__h05e7b949402a010f(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
